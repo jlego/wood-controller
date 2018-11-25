11b8b27934600df60c34590eb959a8abb5f1bc0b
jlego <273734893@qq.com>
2018-11-25 13:19:39 +0800

    no message

--- src/controller.js
+++ src/controller.js
@@ -2,18 +2,18 @@
 // by YuRonghui 2018-4-12
 const { Util } = require('wood-util')();
 const { Query } = require('wood-query')();
+const { catchErr, error } = WOOD;
 
 class Controller {
   constructor(opts = {}, models) {
     this.defaultModel = opts.defaultModel || '';
     this.addLock = opts.addLock || true;
     this.hasCheck = opts.hasCheck || true;
-    this.ctx = opts.ctx || WOOD;
   }
 
   //列表
   async list(req, res, next) {
-    let Model = this.ctx.Plugin('model')._models.get(this.defaultModel),
+    let Model = WOOD.Plugin('model')._models.get(this.defaultModel),
         body = Util.getParams(req),
         page = Number(body.data.page) || 1,
         limit = Number(body.data.limit) || 20,
@@ -21,7 +21,7 @@ class Controller {
     body.data.largepage = largepage;
     let query = Query(body.data).limit(limit);
     let cacheKey = await Util.getListKey(req);
-    const result = await WOOD.catchErr(Model.findList(query, cacheKey, this.addLock));
+    const result = await catchErr(Model.findList(query, cacheKey, this.addLock));
 
     if(result.err){
       res.print(result);
@@ -40,36 +40,36 @@ class Controller {
 
   //详情
   async detail(req, res, next) {
-    let Model = this.ctx.Plugin('model')._models.get(this.defaultModel),
+    let Model = WOOD.Plugin('model')._models.get(this.defaultModel),
         body = Util.getParams(req);
-    const result = await WOOD.catchErr(Model.findOne(body.data, this.addLock));
+    const result = await catchErr(Model.findOne(body.data, this.addLock));
     res.print(result);
   }
 
   //新增
   async create(req, res, next) {
-    let Model = this.ctx.Plugin('model')._models.get(this.defaultModel),
+    let Model = WOOD.Plugin('model')._models.get(this.defaultModel),
         body = Util.getParams(req),
         result = {};
     if(Array.isArray(body.data)){
       for(let i = 0, lang = body.data.length; i < lang; i++){
-        result = await WOOD.catchErr(Model.create(body.data[i], this.addLock, this.hasCheck));
+        result = await catchErr(Model.create(body.data[i], this.addLock, this.hasCheck));
       }
     }else{
-      result = await WOOD.catchErr(Model.create(body.data, this.addLock, this.hasCheck));
+      result = await catchErr(Model.create(body.data, this.addLock, this.hasCheck));
     }
     res.print(result);
   }
 
   //修改
   async update(req, res, next) {
-    let Model = this.ctx.Plugin('model')._models.get(this.defaultModel),
+    let Model = WOOD.Plugin('model')._models.get(this.defaultModel),
         body = Util.getParams(req);
     if(Array.isArray(body.data)){
       let allResult = {};
       for(let i = 0, lang = body.data.length; i < lang; i++){
         delete body.data[i].updateTime;
-        let result = await Util.catchErr(Model.update(body.data[i], this.addLock, this.hasCheck));
+        let result = await catchErr(Model.update(body.data[i], this.addLock, this.hasCheck));
         if(result.err) {
           allResult.err = result.err;
           break;
@@ -81,25 +81,25 @@ class Controller {
       res.print(allResult);
     }else{
       delete body.data.updateTime;
-      const result = await Util.catchErr(Model.update(body.data, this.addLock, this.hasCheck));
+      const result = await catchErr(Model.update(body.data, this.addLock, this.hasCheck));
       res.print(result);
     }
   }
 
   // 删除
   async remove(req, res, next) {
-    let Model = this.ctx.Plugin('model')._models.get(this.defaultModel),
+    let Model = WOOD.Plugin('model')._models.get(this.defaultModel),
         body = Util.getParams(req);
-    const result = await Util.catchErr(Model.remove(body.data));
+    const result = await catchErr(Model.remove(body.data));
     res.print(result);
   }
 
   // 软删除
   async softRemove(req, res, next) {
-    let Model = this.ctx.Plugin('model')._models.get(this.defaultModel),
+    let Model = WOOD.Plugin('model')._models.get(this.defaultModel),
         body = Util.getParams(req);
     body.data.status = -1;
-    const result = await Util.catchErr(Model.update(body.data, this.addLock, this.hasCheck));
+    const result = await catchErr(Model.update(body.data, this.addLock, this.hasCheck));
     res.print(result);
   }
 }
